f66545c4200a0c0e5797d6bae59b26cb7df8210f
EgorikErml <egor.ermlv@mail.ru>
2025-01-31 13:32:53 +0300

    Связка

--- base/classif.py
+++ base/classif.py
@@ -1,27 +1,23 @@
-class Classification:
+class Classification: #Здесь создается класс Classification, который будет использоваться для классификации результатов проверки различных аспектов письма
     def __init__(self):
-        self.resultLinkCheck = None
-        self.resultDomainCheck = None
-        self.resultTextAnalyze = None
-        self.resultClassification = None
-
-    def set_result_link_check(self, result: str):
-        self.resultLinkCheck = result
-
-    def set_result_domain_check(self, result: str):
-        self.resultDomainCheck = result
-
-    def set_result_text_analyze(self, result: bool):
-        self.resultTextAnalyze = result
-
-    def classify(self):
+        self.resultLinkCheck = None #ссылка
+        self.resultDomainCheck = None #домен
+        self.resultTextAnalyze = None #текст анализ
+        self.resultClassification = None #классификация
+    def set_result_link_check(self, result: str): #функция
+        self.resultLinkCheck = result #ссылка
+    def set_result_domain_check(self, result: str):#функция
+        self.resultDomainCheck = result #домен
+    def set_result_text_analyze(self, result: bool):#функция
+        self.resultTextAnalyze = result #текст
+    def classify(self):#функция
         if self.resultDomainCheck == 'Фишинговый' or self.resultLinkCheck == "Фишинговая": #ссылка или  домен
-            self.resultClassification = 'Опасное'
-            return "Опасное"
-        elif (self.resultDomainCheck == "Недавно зарегистрирован"
+            self.resultClassification = 'Опасное' #опасный
+            return "Опасное" #возвращаем
+        elif (self.resultDomainCheck == "Недавно зарегистрирован" #ничего страшного
               or self.resultTextAnalyze == 'Фишинговый' or self.resultLinkCheck == "Подозрительная"): #домен ссылка   или текста
-            self.resultClassification = 'Подозрительное'
-            return "Подозрительное"
+            self.resultClassification = 'Подозрительное' #всё плохо
+            return "Подозрительное" #возвращаем #всё плохо
         else:
-            self.resultClassification = 'Безопасное'
-            return "Безопасное"
+            self.resultClassification = 'Безопасное' #всё отлично
+            return "Безопасное" #возвращаем
