cdc2e310cf52c3949d4ba69e80481254c8541682
EgorikErml <egor.ermlv@mail.ru>
2025-01-31 13:26:25 +0300

    Исправление ошибок

--- base/textanalysis.py
+++ base/textanalysis.py
@@ -5,10 +5,10 @@ from .email_model import Email # Импортируем класс Email для 
 
 class TextAnalysis:#класс
     def __init__(self):
-        # Загружаем модель и токенизатор для классификации фишинговых  писем
+        # загружаем модель и токенизатор для классификации фишинговых  писем
         current_dir = os.path.dirname(os.path.abspath(__file__))   #d
 
-        # Загрузка токенизатора и модели для детектирования  фишинговых писем
+        # загрузка токенизатора и модели для детектирования  фишинговых писем
         self.tokenizer = AutoTokenizer.from_pretrained("cybersectony/phishing-email-detection-distilbert_v2.1") #token
         self.model = AutoModelForSequenceClassification.from_pretrained(  #model
             "cybersectony/phishing-email-detection-distilbert_v2.1") #модель
@@ -17,34 +17,33 @@ class TextAnalysis:#класс
         #Возвращает True, если письмо фишинговое, иначе False.
         # Извлекаем текст из  email
         text = email.text #текст
-        # Преобразуем текст письма в формат,  который принимает  модель
+        # преобразуем текст письма в формат,  который принимает  модель
         inputs = self.tokenizer( #токен
             text,  #текст
-            return_tensors="pt",  # Формат для  PyTorch
-            truncation=True,  # Обрезка текста, если он слишком  длинный
-            max_length=512  # Максимальная длина  текста
-        ) 
-        # Получаем предсказание  модели
-        with torch.no_grad(): # Отключаем вычисление градиентов, так как это только предсказание
+            return_tensors="pt",  # формат для  PyTorch
+            truncation=True,  # обрезка текста, если он слишком  длинный
+            max_length=512  # максимальная длина  текста
+        )
+        # получаем предсказание  модели
+        with torch.no_grad(): # отключаем вычисление градиентов, так как это только предсказание
             outputs = self.model(**inputs) # Передаем данные в модель
             predictions = torch.nn.functional.softmax(outputs.logits, dim=-1) # Применяем softmax для вероятностей
-        # Получаем вероятности  для каждого класса
+        # получаем вероятности  для каждого класса
         probs = predictions[0].tolist() #используется для преобразования тензора (или многомерного массива) в список
-        # Создаем  словарь с результатами
-        labels = {
-            "legitimate_email": probs[0],
-            "phishing_url": probs[1],
-            "legitimate_url": probs[2],
-            "phishing_url_alt": probs[3]
-        }
-        max_label = max(labels.items(), key=lambda x: x[1])
-        if max_label[0] == "phishing_url":
-            #  print(text)
-            #  print('Фишинговый текст')
-            email.classification.set_result_text_analyze('Фишинговый')
-        else:
-            #  print(text)
-            #  print('Безопасный текст')
-            email.classification.set_result_text_analyze('Без признаков фишинга')
 
-        return max_label[0] == "phishing_url"
+        labels = { # создаем  словарь с результатами
+            "legitimate_email": probs[0], #класс
+            "phishing_url": probs[1], #класс
+            "legitimate_url": probs[2], #класс
+            "phishing_url_alt": probs[3] #класс
+        } #закончили
+        max_label = max(labels.items(), key=lambda x: x[1]) # находим класс с максимальной вероятностью.
+        if max_label[0] == "phishing_url": #проверка
+            #print(text)
+            #  print('фишинговый текст')
+            email.classification.set_result_text_analyze('Фишинговый') #Проверяем, если класс с наибольшей вероятностью — это phishing_url, то считаем письмо фишинговым и присваиваем результат через метод set_result_text_analyze объекта classification у email.
+        else: #иначе
+            #print(text)
+            #  print('безопасный текст')
+            email.classification.set_result_text_analyze('Без признаков фишинга') #Возвращаем результат анализа, где True означает, что письмо фишинговое (max_label[0] == "phishing_url"), а False — что письмо безопасное.
+        return max_label[0] == "phishing_url" #Возвращаем результат анализа
